a7dee8ed4ec33a7daf5b8e93105fd1b5046ac865
irolram <rrivan@iesrafaelalberti.es>
2023-12-14 01:09:49 +0100

    prueba

---
+++ Prtubea/src/Unidad_1_por_ejemplo/Ejercicio1.kt
@@ -0,0 +1,8 @@
+package Unidad_1_por_ejemplo
+
+class Ejercicio1 {
+    fun main(){
+        print("Tus muertos")
+    }
+
+}
---
+++ Prtubea/src/Unidad_1_por_ejemplo/ejercicio2.kt
@@ -0,0 +1,14 @@
+package Unidad_1_por_ejemplo
+
+import java.util.Scanner
+
+class Ejercicio2 {
+}
+
+fun main(){
+    print("tus muertos")
+    println("Please enter the Name: ")
+
+    var name = readLine()!!
+    println("Name is : $name")
+}
