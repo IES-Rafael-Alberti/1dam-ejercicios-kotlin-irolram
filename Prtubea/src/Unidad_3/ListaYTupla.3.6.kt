80c4947a3b4fa862dcea30127b0a402b56841200
irolram <rrivan@iesrafaelalberti.es>
2024-01-15 17:59:59 +0100

    tarea

--- Prtubea/src/Unidad_3/ListaYTupla.3.6.kt
+++ Prtubea/src/Unidad_3/ListaYTupla.3.6.kt
@@ -2,20 +2,21 @@ package Unidad_3
 
 fun main() {
     val asignaturas: MutableList<String> = mutableListOf("Matematicas", "Historia", "Lengua", "Química", "Física")
-    val asignaturasARetirar = mutableListOf<String>()
+    val asignaturasAprobadas = mutableListOf<String>()
 
     for (asignatura in asignaturas.toList()) {
         println("¿Qué nota ha sacado en $asignatura?")
         val nota: Double = readLine()?.toDoubleOrNull() ?: 0.0
 
         if (nota >= 5.0) {
-            asignaturasARetirar.add(asignatura)
+
+            asignaturasAprobadas.add(asignatura)
         } else {
             println("Suspendiste $asignatura")
         }
     }
 
-    asignaturas.removeAll(asignaturasARetirar)
+    asignaturas.removeAll(asignaturasAprobadas)
 
     println("Asignaturas a repetir: $asignaturas")
 }
